51c492494227bdf9cc78f39dff42892c1631f8ca
Thibaud Brault <thibaud.brault@protonmail.com>
2023-07-27 11:36:50 +0200

    :label:move types + create new one

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,1 +1,24 @@
-export * from './experiences';
+export type TStack = {
+  icon: string;
+  name: string;
+};
+
+export type TExperiences = {
+  role: string;
+  company: string;
+  city: string;
+  start: string;
+  end: string;
+  type: string;
+  missions: {
+    client: string;
+    details: string[];
+    stack: TStack[];
+  }[];
+};
+
+export type TImages = {
+  id: number;
+  src: string;
+  alt: string;
+};
