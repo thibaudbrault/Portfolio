981f05fe1d50785f8e3e2ab189aeeebd4969889c
Thibaud Brault <thibaud.brault@protonmail.com>
2023-07-27 11:37:40 +0200

    :recycle: modify imports + update code

--- src/components/bio/Bio.tsx
+++ src/components/bio/Bio.tsx
@@ -1,62 +1,51 @@
-import {
-  AstroIcon,
-  CssIcon,
-  GraphqlIcon,
-  HtmlIcon,
-  JavascriptIcon,
-  MysqlIcon,
-  NextIcon,
-  PrismaIcon,
-  ReactIcon,
-  SassIcon,
-  StyledIcon,
-  TailwindIcon,
-  TypescriptIcon,
-} from '@/lib';
-import type { TStack } from '@/types';
+import type { TStack } from "@/types";
 
-import Title from '../projects/Title.astro';
-import { TechBadge } from '../ui';
+import { Title } from "../projects";
+import { TechBadge } from "../ui";
+import { useMemo } from "react";
 
 export const Bio = () => {
-  const stack: TStack[][] = [
-    [{ name: `HTML`, icon: <HtmlIcon /> }],
-    [
-      { name: `CSS`, icon: <CssIcon /> },
-      { name: `Sass`, icon: <SassIcon /> },
-      { name: `Styled-Components`, icon: <StyledIcon /> },
-      { name: `Tailwind`, icon: <TailwindIcon /> },
+  const stack: TStack[][] = useMemo(
+    () => [
+      [{ name: `HTML`, icon: "logos:html-5" }],
+      [
+        { name: `CSS`, icon: "logos:css-3" },
+        { name: `Sass`, icon: "logos:sass" },
+        { name: `Styled-Components`, icon: "vscode-icons:file-type-styled" },
+        { name: `Tailwind`, icon: "logos:tailwindcss-icon" },
+      ],
+      [
+        { name: `Javascript`, icon: "logos:javascript" },
+        { name: `Typescript`, icon: "logos:typescript-icon" },
+      ],
+      [
+        { name: `React`, icon: "logos:react" },
+        { name: `Next`, icon: "logos:nextjs-icon" },
+        { name: `Astro`, icon: "vscode-icons:file-type-astro" },
+      ],
+      [
+        { name: `Prisma`, icon: "vscode-icons:file-type-prisma" },
+        { name: `MySQL`, icon: "logos:mysql-icon" },
+        { name: `GraphQL`, icon: "logos:graphql" },
+      ],
     ],
-    [
-      { name: `Javascript`, icon: <JavascriptIcon /> },
-      { name: `Typescript`, icon: <TypescriptIcon /> },
-    ],
-    [
-      { name: `React`, icon: <ReactIcon /> },
-      { name: `Next`, icon: <NextIcon /> },
-      { name: `Astro`, icon: <AstroIcon /> },
-    ],
-    [
-      { name: `Prisma`, icon: <PrismaIcon /> },
-      { name: `MySQL`, icon: <MysqlIcon /> },
-      { name: `GraphQL`, icon: <GraphqlIcon /> },
-    ],
-  ];
+    []
+  );
 
   return (
     <section id="bio" className="flex flex-col gap-12">
       <div className="flex flex-col gap-3">
         <Title text="Bio" />
-        <p className="font-raleway mx-auto w-2/3 text-2xl">
+        <p className="mx-auto w-5/6 md:w-2/3 text-2xl text-center">
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et neque
           eligendi dicta libero nam perspiciatis, impedit porro.
         </p>
       </div>
-      <div className="flex justify-center gap-6">
-        {stack.map((tech) => (
-          <ul className="flex flex-col gap-6">
-            {tech.map((t, i) => (
-              <TechBadge tech={t} index={i} />
+      <div className="flex flex-col w-5/6 mx-auto md:flex-row md:w-full justify-center gap-6">
+        {stack.map((tech, index) => (
+          <ul className="flex flex-col gap-6" key={index}>
+            {tech.map((t, techIndex) => (
+              <TechBadge tech={t} index={techIndex} key={techIndex} />
             ))}
           </ul>
         ))}
--- src/components/ui/TechBadge.tsx
+++ src/components/ui/TechBadge.tsx
@@ -1,19 +1,14 @@
-import { Icon } from '@iconify/react';
-import type { HTMLAttributes } from 'react';
+import { Icon } from "@iconify/react";
+import type { HTMLAttributes } from "react";
 
-import type { TStack } from '@/types';
+import type { TStack } from "@/types";
 
-interface TechBadgeProps extends HTMLAttributes<HTMLLIElement> {
+interface Props extends HTMLAttributes<HTMLLIElement> {
   tech: TStack;
   index: number;
 }
 
-export function TechBadge({
-  className,
-  tech,
-  index,
-  ...props
-}: TechBadgeProps) {
+export function TechBadge({ tech, index, ...props }: Props) {
   return (
     <li
       className="flex items-center gap-3 rounded-md border-2 border-gray-700 px-6 py-2 text-lg font-bold transition duration-300 ease-in-out hover:border-yellow-600"
